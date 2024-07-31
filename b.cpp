761a56111f6fda39be6fcbed89b7621f476e491f
Khánh Phạm <davidkhanh206@gmail.com>
2024-07-31 09:33:47 +0700

    Đệ quy test các số

--- b.cpp
+++ b.cpp
@@ -1,21 +1,13 @@
-#include <iostream>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-bool cmp(int a, int b){
-	int r1 = a % 2, r2 = b % 2;
-	if(r1 == 0 && r2 == 1) return false;
-	if(r1 == 1 && r2 == 0) return true;
-	if(r1 == 0 && r2 == 0) return a > b;
-	if(r1 == 1 && r2 == 1) return a<b;
+int fibonancci (int n){
+    if(n<=1){
+        return n;
+    }else{
+        return fibonancci(n-1)+fibonancci(n-2);
+    }
 }
-int main(){
-	int a[] = {1, 2, 10, 9, 6, 5, 8, 7, 4, 3};
-	int n = 10;
-	sort(a, a + n, cmp);
-	cout << "Mang sau khi sap xep : \n";
-	for(int i = 0; i < n; i++){
-		cout << a[i] << " ";
-	}
+int main (){
+    int n;cin>>n;
+    cout<<fibonancci(n);
 }
