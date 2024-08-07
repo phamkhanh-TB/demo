16b4916131017e6bc14d09aa724c362a054adb5d
Khánh Phạm <davidkhanh206@gmail.com>
2024-08-07 14:40:47 +0700

    sửa tý

---
+++ demo.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct date {
+    int date;
+    int month;
+    int year;
+    void nhap(){
+        cin>>date>>month >>year;
+    } 
+    void xuat(){
+        cout<<date<<month<<year;
+    }
+};
+int main(){
+    date a;
+    a.nhap();
+    a.xuat();
+}
