7f50197e51e1f7e84b50759fe7de66a9175261c1
Khánh Phạm <davidkhanh206@gmail.com>
2024-08-07 15:34:27 +0700

    ed

--- demo.cpp
+++ demo.cpp
@@ -1,18 +1,2 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct date {
-    int date;
-    int month;
-    int year;
-    void nhap(){
-        cin>>date>>month >>year;
-    } 
-    void xuat(){
-        cout<<date<<month<<year;
-    }
-};
-int main(){
-    date a;
-    a.nhap();
-    
-}
