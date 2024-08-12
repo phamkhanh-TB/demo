a81173a58a3743954f83209a6be9ccf398c5eb39
Khánh Phạm <davidkhanh206@gmail.com>
2024-08-12 08:53:03 +0700

    thay đổi tý

--- ex2.cpp
+++ ex2.cpp
@@ -1,17 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool find_sum(vector<int> &v, const int &k){
-    for(int i=0;i<v.size()-1;i++){
-        for(int j=i+1;j<v.size();j++){
-            int sum=v[i]+v[j];
-            if(sum==k) return true;
-        }
-        
-    }
-    return false;
-}
 int main(){
-    vector<int> arr(6);
-    for(int &x:arr) cin>>x;
-    (find_sum(arr,50)==true)?cout<<"yes":cout<<"no";
+    //  int n,m;cin>>n>>m;
+    // vector<vector<int>> arr(n,vector<int> (m));
+    // for( int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cin>>arr[i][j];
+    //     }
+    // }
+    // for( int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cout<<arr[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    int soHang, soCot;
+    cout << "Nhap so hang: ";
+    cin >> soHang;
+    cout << "Nhap so cot: ";
+    cin >> soCot;
+
+    // Khai báo và khởi tạo mảng hai chiều
+    vector<vector<int>> mangHaiChieu(soHang, vector<int>(soCot));
+
+    // Nhap gia tri cho tung phan tu
+    for (int i = 0; i < soHang; ++i) {
+        for (int j = 0; j < soCot; ++j) {
+            cout << "Nhap a[" << i << "][" << j << "]: ";
+            cin >> mangHaiChieu[i][j];
+        }
+    }
+
+    // In ra mảng đã nhập
+    for (const auto& hang : mangHaiChieu) {
+        for (int phanTu : hang) {
+            cout << phanTu << " ";
+        }
+        cout << endl;
+    }
 }
